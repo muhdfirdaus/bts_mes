654392f32545ca1111a6c905bb091646bdc8cf64
muhdfirdaus <dausmomo5779@gmail.com>
2019-01-10 09:59:16 +0800

    finalVMI update, error fixed.

--- pages/finalVMI_in.php
+++ pages/finalVMI_in.php
@@ -8,14 +8,23 @@ $limit = $_POST['limit'];
 $id = $_SESSION['id'];
 $tmstmp = time();
 
-
+$incompleteData = 0;
 for($i=1; $i<=$limit; $i++){
     
     $sn=trim($_POST['sn'.$i], " ");
     $result=trim($_POST['result'.$i], " ");
 
-    if(strlen($sn)>1)
+    if(strlen($sn)<=1)
     {    
+        $incompleteData+=1;
+    }
+}
+
+if($incompleteData == 0){
+    for($i=1; $i<=$limit; $i++){
+        $sn=trim($_POST['sn'.$i], " ");
+        $result=trim($_POST['result'.$i], " ");
+
         $query=mysqli_query($con,"select count(*) as cnt from sn_master where sn='$sn'")or die(mysqli_error($con));
         $row=mysqli_fetch_array($query);
         if($row['cnt']!=0){
@@ -25,15 +34,15 @@ for($i=1; $i<=$limit; $i++){
             mysqli_query($con,"INSERT INTO sn_master(sn,fvmiTest, fvmiTime, fvmiTesterID, lastUpdate)VALUES('$sn','$result','$tmstmp','$id','$tmstmp')")or die(mysqli_error($con));
         }
     }
-    else
-    {
-        echo "<script type='text/javascript'>alert('Please fill all column!');</script>";
-        echo "<script>window.history.back();</script>"; 
-    }
+    echo "<script type='text/javascript'>alert('Data saved!');</script>";
+    echo "<script type='text/javascript'>document.location='finalVMI.php'</script>";
+}
+else{
+    echo "<script type='text/javascript'>alert('Please fill all column!');</script>";
+    echo "<script>window.history.back();</script>"; 
 }
 
-echo "<script type='text/javascript'>alert('Data saved!');</script>";
-echo "<script type='text/javascript'>document.location='finalVMI.php'</script>";
+
 
 
 
